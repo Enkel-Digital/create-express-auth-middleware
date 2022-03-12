a6f073937f8cfa7523024d1fb4e494bd8208f02b
Jaime <jaimeloeuf@gmail.com>
2022-03-12 22:01:14 +0800

    Export base function: 1.  Export base function so that other libraries can use this library as     the base to build custom authentication and authorization libs. 2.  Bump minor version for backwards compatible change.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-// Authentication and authorization middlewares using Firebase Auth
+// Export the factory function so that other libraries can build custom versions using this as a base
+export * from "./auth";
 
+// Default authentication and authorization middleware creators
 export * from "./authn";
 export * from "./authz";
